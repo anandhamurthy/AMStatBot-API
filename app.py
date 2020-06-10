542f263e5cd9dc71b8aadedb0bfac00d47f656bf
Anandha Murthy B <anandhamurthy2000@gmail.com>
2020-06-10 16:23:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-#from nltk.downloader import download, download_shell
 import nltk
 nltk.download('punkt')
 nltk.download('wordnet')
@@ -12,6 +11,46 @@ import pandas as pd
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
+symbols=pd.read_csv('tickers.csv')
+
+def check_symbol(sym):
+    for i in symbols['Symbol']:
+        if sym==i:
+            return True
+
+def check_fake(msg):
+    r = requests.post(url='https://amfakeclass.herokuapp.com/predict/'+msg)
+    if r.text=='True':
+        return 'Fake'
+    else:
+        return 'Real'
+
+def check_spam(msg):
+    r = requests.post(url='https://amspamclaa.herokuapp.com/predict/' + msg)
+    if r.text == 'True':
+        return 'Spam'
+    else:
+        return 'Ham'
+
+def check(msg):
+    if msg[0]=='%':
+        if check_symbol(msg[1:]):
+            return 'Stock'
+        else:
+            return 'Invalid Symbol'
+    elif msg[0]=='_':
+        if 'fake' in msg[1:]:
+            if check_fake(msg[5:]):
+                return 'Fake'
+            else:
+                return 'Real'
+        elif 'spam'in msg[1:]:
+            if check_fake(msg[5:]):
+                return 'Fake'
+            else:
+                return 'Real'
+    else:
+        return 'Chat'
 
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
@@ -26,39 +65,53 @@ def bag_of_words(s, words):
 
     return numpy.array(bag)
 
-@app.route('/predict/<msg>',methods=['GET','POST'])
+@app.route('/predict/<msg>',methods=['POST'])
 def predict(msg):
-    data = pd.read_json('chat_intents.json')
-    words = []
-    labels = []
-    docs_x = []
-    docs_y = []
 
-    stemmer = WordNetLemmatizer()
+    if check(msg)=='Chat':
+        data = pd.read_json('chat_intents.json')
+        words = []
+        labels = []
+        docs_x = []
+        docs_y = []
 
-    for intent in data["intents"]:
-        for pattern in intent["patterns"]:
-            wrds = nltk.word_tokenize(pattern)
-            words.extend(wrds)
-            docs_x.append(wrds)
-            docs_y.append(intent["tag"])
+        stemmer = WordNetLemmatizer()
 
-        if intent["tag"] not in labels:
-            labels.append(intent["tag"])
+        for intent in data["intents"]:
+            for pattern in intent["patterns"]:
+                wrds = nltk.word_tokenize(pattern)
+                words.extend(wrds)
+                docs_x.append(wrds)
+                docs_y.append(intent["tag"])
 
-    words = [stemmer.lemmatize(w.lower()) for w in words if w != "?"]
-    words = sorted(list(set(words)))
+            if intent["tag"] not in labels:
+                labels.append(intent["tag"])
 
-    labels = sorted(labels)
-    results = model.predict([bag_of_words(msg, words)])
-    results_index = numpy.argmax(results)
-    tag = labels[results_index]
+        words = [stemmer.lemmatize(w.lower()) for w in words if w != "?"]
+        words = sorted(list(set(words)))
 
-    for tg in data["intents"]:
-        if tg['tag'] == tag:
-            responses = tg['responses']
+        labels = sorted(labels)
+        results = model.predict([bag_of_words(msg, words)])
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
 
-    return (random.choice(responses))
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+
+        return (random.choice(responses))
+    elif check(msg)=='Stock':
+        return ('https://amstock.herokuapp.com/'+msg[1:])
+    elif check(msg)=='Fake':
+        if check_fake(msg):
+            return ('True')
+        else:
+            return ('False')
+    elif check(msg)=='Spam':
+        if check_spam(msg):
+            return ('True')
+        else:
+            return ('False')
 
 if __name__ == "__main__":
     app.run(debug=True)
