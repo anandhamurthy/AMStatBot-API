69caae7231120978e029bce884b88247cb362664
Anandha Murthy B <anandhamurthy2000@gmail.com>
2021-05-22 14:53:50 +0530

    Update app.py
    changed param to body request

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
+import pandas_datareader.data as web
+import datetime as dt
+import requests
+import pandas as pd
+import pickle
+from flask import Flask, request, jsonify, render_template
+from nltk.stem import WordNetLemmatizer
+import random
+import numpy
 import nltk
 nltk.download('punkt')
 nltk.download('wordnet')
-import numpy
-import random
-from nltk.stem import WordNetLemmatizer
-stemmer=WordNetLemmatizer()
-from flask import Flask, request, jsonify, render_template
-import pickle
-import pandas as pd
-import requests
-import datetime as dt
-import pandas_datareader.data as web
+stemmer = WordNetLemmatizer()
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
-symbols=pd.read_csv('Symbols.csv')
+symbols = pd.read_csv('Symbols.csv')
+
 
 def get_name(msg):
     for i in range(len(symbols['Symbol'])):
-        if msg==symbols['Symbol'][i]:
+        if msg == symbols['Symbol'][i]:
             return symbols['Company'][i]
     else:
         return msg
 
-def pct_change(open_price,current_price):
+
+def pct_change(open_price, current_price):
     pct = ((current_price-open_price)/open_price)*100
     return float(pct)
+
 
 def get_details(msg):
     start_date = dt.datetime(2015, 1, 1)
@@ -33,33 +36,45 @@ def get_details(msg):
     df = web.DataReader(msg, 'yahoo', start_date, end_date)
     df.reset_index(inplace=True)
     df.set_index("Date", inplace=True)
-    return jsonify(
-        name=get_name(msg),
-        website='https://amstock.herokuapp.com/' + msg,
-        close=float(df['Close'][-1]),
-        open=float(df['Open'][-1]),
-        high=float(df['High'][-1]),
-        low=float(df['Low'][-1]),
-        volume=float(df['Volume'][-1]),
-        percent=pct_change(df['Close'][-2], df['Close'][-1]),
-        symbol=msg
-    )
+
+    stock_response = {
+        "name": get_name(msg),
+        "website": 'https://amstock.herokuapp.com/' + msg,
+        "close": float(df['Close'][-1]),
+        "open": float(df['Open'][-1]),
+        "high": float(df['High'][-1]),
+        "low": float(df['Low'][-1]),
+        "volume": float(df['Volume'][-1]),
+        "percent": pct_change(df['Close'][-2], df['Close'][-1]),
+        "symbol": msg
+    }
+
+    response = {
+        'status': 200,
+        'message': 'OK',
+        'response': stock_response
+
+    }
+    return jsonify(response)
+
 
 def check_symbol(sym):
     for i in symbols['Symbol']:
-        if sym==i:
+        if sym == i:
             return True
 
-def check(msg):
-    if msg[0]=='_':
-        if 'stock' in msg[1:]:
-            return 'Stock'
-        elif 'fake' in msg[1:]:
-            return 'Fake'
-        elif 'spam'in msg[1:]:
-            return 'Spam'
-    else:
-        return 'Chat'
+
+# def check(msg):
+#     if msg[0] == '_':
+#         if 'stock' in msg[1:]:
+#             return 'Stock'
+#         elif 'fake' in msg[1:]:
+#             return 'Fake'
+#         elif 'spam' in msg[1:]:
+#             return 'Spam'
+#     else:
+#         return 'Chat'
+
 
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
@@ -74,10 +89,13 @@ def bag_of_words(s, words):
 
     return numpy.array(bag)
 
-@app.route('/predict/<msg>',methods=['GET','POST'])
-def predict(msg):
 
-    if check(msg)=='Chat':
+@app.route('/predict/', methods=['GET', 'POST'])
+def predict():
+    msg = request.form.get('message', type=str)
+    type_id = request.form.get('type', type=int)
+    print(msg, type_id)
+    if type_id == 1:
         data = pd.read_json('chat_intents.json')
         words = []
         labels = []
@@ -108,12 +126,18 @@ def predict(msg):
             if tg['tag'] == tag:
                 responses = tg['responses']
 
-        return (random.choice(responses))
-    elif check(msg)=='Stock':
+        response = {
+            'status': 200,
+            'message': 'OK',
+            'response': random.choice(responses)
+        }
+        return jsonify(response)
+    elif type_id == 2:
         if check_symbol(msg[6:]):
             return get_details(msg[6:])
         else:
             return 'Invalid'
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
